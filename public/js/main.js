93034b49fbeaa231464dab578fa18bc14e7e6f16
Shukhrat <sh.khaydarov96@gmail.com>
2020-07-19 18:55:15 +0200

    Update the message shown to user before deletion

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,10 @@ const users=document.getElementById("users");
 if(users){
     users.addEventListener('click', e => {
         if(e.target.className==='btn btn-danger delete-user'){
-            if(confirm('Sure?')){
+            if(confirm("Confirm the remove of user with OK button, else press Cancel.")){
                 const id=e.target.getAttribute('data-id');
 
-                alert(id);
+                alert("Deleted user with id: "+id);
 
                 fetch(`/symphuser/public/user/delete/${id}`, {
                     method: 'DELETE'
